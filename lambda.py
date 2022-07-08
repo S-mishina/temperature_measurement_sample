2e2888ec68370ddc72661aa22bc4c0a62e2be9b0
S-mishina <seiryu.mishina@zozo.com>
2022-07-09 03:44:53 +0900

    change code Lambda.py

--- lambda.py
+++ lambda.py
@@ -1,17 +1,26 @@
+from datetime import datetime
+from boto3.dynamodb.conditions import Key, Attr
 import json
+import boto3
 
 def lambda_handler(event, context):
     print('---')
     print(event)
-    try:
-        if event['temperature']!='':
-            #TODO:ここからdynamoDBに書き込み
-            return {
-                'statusCode': 200,
-                'body': str(event['temperature'])
-            }
-    except:
+    if len(event)!=0:
+        #TODO:環境変数に置き換える
+        dynamodb = boto3.resource('')
+        table = dynamodb.Table('')
+        now = datetime.today()
+        day=now.strftime('%Y/%m/%d')
+        date=now.strftime('%H:%M:%S')
+        #ここでデータを追加する。
+        table.put_item(Item={'day': str(day) ,'date': str(date),'temperature':str(event['temperature'])})
         return {
-                'statusCode': 200,
-                'body': str(event)
+            'statusCode': 200,
+            'body': str(event['temperature'])
+        }
+    else:
+        return {
+            'statusCode': 200,
+            'body': "no data"
         }
